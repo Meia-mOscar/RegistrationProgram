b3a990c79aa6f55cceb4dd806212d3f151d84667
Oscar van der Westhuixen <oscar.vdw99@gmail.com>
2024-07-08 21:59:51 +0200

    Save as XML feature added

--- RegistrationListWriter.cpp
+++ RegistrationListWriter.cpp
@@ -10,13 +10,49 @@ bool RegistrationListWriter::writeToFile(RegistrationList &regList, QString &fil
     doc.appendChild(root);
 
     for(int i=0; i<regList.totalRegistrations("allRegistrations"); i++){
-        QDomElement regElement = doc.createElement("Registration");
-        regElement.setAttribute("Name", regList.at(i)->getAttendee().getName());
-        regElement.setAttribute("Affiliation", regList.at(i)->getAttendee().getAffiliation());
-        regElement.setAttribute("Email", regList.at(i)->getAttendee().getEmail());
-        regElement.setAttribute("Type", regList.at(i)->getType());
-        regElement.setAttribute("Fee", QString::number(regList.at(i)->calculateFee()));
+        QDomElement regElement = doc.createElement("registration");
+        regElement.setAttribute("type", regList.at(i)->getType());
+
+        QDomElement attendeeElement = doc.createElement("attendee");
+
+        QDomElement nameElement = doc.createElement("name");
+        QDomText nameText = doc.createTextNode(regList.at(i)->getAttendee().getName());
+        nameElement.appendChild(nameText);
+        attendeeElement.appendChild(nameElement);
+
+        QDomElement affiliationElement = doc.createElement("affiliation");
+        QDomText affiliationText = doc.createTextNode(regList.at(i)->getAttendee().getAffiliation());
+        affiliationElement.appendChild(affiliationText);
+        attendeeElement.appendChild(affiliationElement);
+
+        QDomElement emailElement = doc.createElement("email");
+        QDomText emailText = doc.createTextNode(regList.at(i)->getAttendee().getEmail());
+        emailElement.appendChild(emailText);
+        attendeeElement.appendChild(emailElement);
+
+        regElement.appendChild(attendeeElement);
+
+        QDomElement bookingDateElement = doc.createElement("bookingdate");
+        QDomText bookingDateText = doc.createTextNode(regList.at(i)->getBookingDate().toString());
+        bookingDateElement.appendChild(bookingDateText);
+        regElement.appendChild(bookingDateElement);
+
+        QDomElement registrationFeeElement = doc.createElement("registrationfee");
+        QDomText registrationFeeText = doc.createTextNode(QString::number(regList.at(i)->calculateFee()));
+        registrationFeeElement.appendChild(registrationFeeText);
+        regElement.appendChild(registrationFeeElement);
+
+        root.appendChild(regElement);
     }
 
-    return true;
+    QFile file(fileName);
+    if(!file.open(QIODevice::WriteOnly)){
+        return false;
+    }else{
+        QTextStream stream(&file);
+        stream << doc.toString();
+        file.close();
+        return true;
+    }
+
 }
